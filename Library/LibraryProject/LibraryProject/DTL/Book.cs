0dbdd544312d9c3d97f67a3ce2e95b9a41264af3
StarLor4 <135303869+StarLor4@users.noreply.github.com>
2024-10-20 15:16:05 +0300

    Add files via upload

---
+++ Library/LibraryProject/LibraryProject/DTL/Book.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace LibraryProject.DTL
+{
+    class Book
+    {
+        private int bookNumber;
+        private string title;
+        private string authors;
+        private string publisher;
+
+        public int BookNumber { get => bookNumber; set => bookNumber = value; }
+        public string Title { get => title; set => title = value; }
+        public string Authors { get => authors; set => authors = value; }
+        public string Publisher { get => publisher; set => publisher = value; }
+    }
+}
